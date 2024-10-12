f171a39a89ec83782430176780bdc7bda19c1369
Puneet Bajaj <puneetbajaj591@gmail.com>
2024-10-12 17:22:23 -0400

    vercel error fix attempt 2

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
+export const runtime = 'nodejs';
+
 export async function POST(request: NextRequest) {
   try {
     const { name, email, message } = await request.json();
